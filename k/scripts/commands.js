f53e27c9541c9424a4732570a5edb3dee471c04f
koroorrarokair <40911271+kieroid@users.noreply.github.com>
2023-06-29 21:13:54 +0200

    update commands

--- k/scripts/commands.js
+++ k/scripts/commands.js
@@ -1,5 +1,8 @@
-
+function version() {
+    return text("shellfish.racing v2.0.0 - 20230629")
+}
 function help(args) {
+    if ()
     return text(Object.keys(commands).join(", ") +"\n\nuse --help to get more help on a specific command")
 }
 function echo(args) {return text(args.join(" "))}
@@ -32,10 +35,19 @@ or any ASCII file of your choice.`)
     } else {
         return html(`
             <div class="neofetch">
-                <img class="neofetch-img u-logo" src="k/images/groggy.png" width="128" height="128">
+                <img class="neofetch-img u-logo" src="k/images/hand.jpg" width="128" height="128">
                 <div class="neofetch-text">
-                    <b class="">k</b>@<b class="magenta">shellfish.racing</b><br>
-                    -------------------<br>
+                    <b class=""><a href="/k/gpgpub.txt">kiroid</a></b>@<b class="magenta">shellfish.racing</b><br>
+                    ------------------------<br>
+                    hey there. i am "big k" "special k" or whatever u call me.
+                    the internet is crazy, and so i got bored one day so i made this. <br>
+                    on the topic of THIS stuff, i know a few programming languages (i think.) 
+                    <b>python, java, javascript, css</b>(?), <b>html</b>(?), <b>bash</b>(?)
+                    i dunno really. whatever it is, feel free to take a look.
+                    <br><br>
+                    <b>pronouns: </b>they/them<br>
+                    <b>links: </b><a href="https://github.com/kieroid">github</a> <a href="https://open.spotify.com/user/kieranlavato">spotify</a> <a href="/k/gpgpub.txt">pgp</a><br>
+                    <b>contact: </b><a href="https://discord.com/invite/HynFCu5eax">discord</a> <a href="mailto: k@shellfish.racing">email</a>  <a href="https://matrix.im/#/@kiramu">matrix.org</a>
                 </div>
             </div>
         `);
@@ -73,5 +85,6 @@ const commands = {
     "echo": echo,
     "clear": clear,
     "sudo": sudo,
-    "neofetch": neofetch
+    "neofetch": neofetch,
+    "version": version
 };
