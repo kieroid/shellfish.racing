94ff92fb9553366e91962e7d2dd6efb73ae5a3bb
koroorrarokair <40911271+kieroid@users.noreply.github.com>
2023-09-22 15:47:11 -0500

    update server id

--- k/scripts/commands.js
+++ k/scripts/commands.js
@@ -133,7 +133,7 @@ function neofetch(args) {
                 i dunno really. whatever it is, feel free to take a look.
                 <br><br>
                 <b>links: </b><a href="https://github.com/kieroid">github</a> <a href="https://open.spotify.com/user/kieranlavato">spotify</a> <a href="/k/gpgpub.txt">pgp</a><br>
-                <b>contact: </b><a href="https://discord.gg/H4JZbBc98f">discord</a> <a href="mailto: k@shellfish.racing">email</a> <a href="https://matrix.to/#/@kiramu:matrix.org">matrix.org</a>
+                <b>contact: </b><a href="https://discord.gg/Jxt345xFVk">discord</a> <a href="mailto: k@shellfish.racing">email</a> <a href="https://matrix.to/#/@kiramu:matrix.org">matrix.org</a>
             </div>
         </div>
     `);
